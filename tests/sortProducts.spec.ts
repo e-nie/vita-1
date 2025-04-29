7a19b8f349df7e60adcd6c0049c6c3bffb8e863f
e-nie <eniehaus111@gmail.com>
2025-04-29 19:38:50 +0200

    fix sort poll

--- tests/sortProducts.spec.ts
+++ tests/sortProducts.spec.ts
@@ -9,13 +9,13 @@ test.describe('Sort products functionality - POSITIVE', () => {
   });
 
   test('should sort products by name in ascending order', async ({ homePage }) => {
-    await homePage.sortSelect.sortBy('name,asc');// sort products on the page
+    await homePage.sortSelect.sortBy('name,asc'); // sort products on the page
 
     await expect
       .poll(async () => {
-        const productsOnThePage = await homePage.getProductNames();// ['a', 'b', 'c']// get sorted products on the page
+        const productsOnThePage = await homePage.getProductNames(); // ['a', 'b', 'c']// get sorted products on the page
         const sortedProductsOnThePage = [...productsOnThePage].sort(); // ['a', 'b', 'c'] sorts same before sorted products with js
-        return areEqualShallow(productsOnThePage, sortedProductsOnThePage);// compare sorted products from page  with products sorted with js
+        return areEqualShallow(productsOnThePage, sortedProductsOnThePage); // compare sorted products from page  with products sorted with js
       })
       .toEqual(true);
   });
@@ -26,7 +26,7 @@ test.describe('Sort products functionality - POSITIVE', () => {
     await expect
       .poll(async () => {
         const productsOnThePage = await homePage.getProductNames();
-            const sortedProductsOnThePage = [...productsOnThePage].sort((a: string, b: string) =>
+        const sortedProductsOnThePage = [...productsOnThePage].sort((a: string, b: string) =>
           b.localeCompare(a),
         ); // ['a', 'b', 'c']
         return areEqualShallow(productsOnThePage, sortedProductsOnThePage);
@@ -37,41 +37,36 @@ test.describe('Sort products functionality - POSITIVE', () => {
   test('should sort products by price in ascending order', async ({ homePage }) => {
     //sort the products on the page
     await homePage.sortSelect.sortBy('price,asc');
-    //get the prices from the page
-    const pricesOnThePage = await homePage.getProductPrices();
-    //remove non-numeric characters and convert to numbers
-    const pricesAsNumbers = [...pricesOnThePage].map(p=> {
-      const price = p.replace(/[^0-9.]/g, ''); // Remove non-numeric characters
-      return parseFloat(price);
-    })
 
-    console.log(pricesAsNumbers)
     await expect
       .poll(async () => {
+        //get the prices from the page
+        const pricesOnThePage = await homePage.getProductPrices();
+        //remove non-numeric characters and convert to numbers
+        const pricesAsNumbers = [...pricesOnThePage].map((p) => {
+          const price = p.replace(/[^0-9.]/g, ''); // Remove non-numeric characters
+          return parseFloat(price);
+        });
         const sortedPricesAsNumbers = [...pricesAsNumbers].sort((a: number, b: number) => a - b); // Sort the prices in ascending order
-        console.log(sortedPricesAsNumbers)
         return areEqualShallow(pricesAsNumbers, sortedPricesAsNumbers);
       })
       .toEqual(true);
   });
 
-
-
-
   test('should sort products by price in descending order', async ({ homePage }) => {
     await homePage.sortSelect.sortBy('price,desc');
-    const pricesOnThePage = await homePage.getProductPrices();
-    const pricesAsNumbers = [...pricesOnThePage].map(p=> {
-      const price = p.replace(/[^0-9.]/g, ''); 
-      return parseFloat(price);
-    })
+   
 
     await expect
-    .poll(async () => {
-      const sortedPricesAsNumbers = [...pricesAsNumbers].sort((a: number, b: number) => b - a); // Sort the prices in descending order
-      console.log(sortedPricesAsNumbers)
-      return areEqualShallow(pricesAsNumbers, sortedPricesAsNumbers);
-    })
-    .toEqual(true);
+      .poll(async () => {
+        const pricesOnThePage = await homePage.getProductPrices();
+        const pricesAsNumbers = [...pricesOnThePage].map((p) => {
+          const price = p.replace(/[^0-9.]/g, '');
+          return parseFloat(price);
+        });
+        const sortedPricesAsNumbers = [...pricesAsNumbers].sort((a: number, b: number) => b - a); // Sort the prices in descending order
+        return areEqualShallow(pricesAsNumbers, sortedPricesAsNumbers);
+      })
+      .toEqual(true);
   });
 });
